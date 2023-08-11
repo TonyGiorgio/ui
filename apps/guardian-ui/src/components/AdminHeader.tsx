a1d321a3bb9f38417a120cc8524b7db4107f08ad
Ethan Tuttle <tuttle.ethan@protonmail.com>
2023-08-11 17:33:40 -0400

    feat: add coloration to guardians status indicator
    Indicates Red for less than 2/3 guardians online, yellow for 2/3 or more online. And green otherwise.

--- apps/guardian-ui/src/components/AdminHeader.tsx
+++ apps/guardian-ui/src/components/AdminHeader.tsx
@@ -1,16 +1,23 @@
 import React, { FC, useEffect, useState } from 'react';
 import { CopyInput } from '@fedimint/ui';
 import { useAppContext, useSetupContext } from '../hooks';
-import { Flex, Box, Text, Icon } from '@chakra-ui/react';
+import { Flex, Box, Text, Icon, useColorModeValue } from '@chakra-ui/react';
 import { ReactComponent as CopyIcon } from '../assets/svgs/copy.svg';
 
 interface PillProp {
   text: string;
   status: string;
+  backgroundColor?: string;
+  statusColor?: string;
 }
 
 // Have to update pull color based on quorum.
-export const Pill: FC<PillProp> = ({ text, status }) => {
+export const Pill: FC<PillProp> = ({
+  text,
+  status,
+  backgroundColor,
+  statusColor,
+}) => {
   return (
     <Flex gap='8px' alignItems='center'>
       <Text color='#6B7280' lineHeight='27px' fontSize='14px'>
@@ -21,11 +28,11 @@ export const Pill: FC<PillProp> = ({ text, status }) => {
         p='2px 10px'
         borderRadius='32px'
         alignItems='center'
-        bgColor='#D1FAE5'
+        bgColor={backgroundColor ?? '#D1FAE5'}
       >
         <Box bgColor='#34D399' h='8px' w='8px' borderRadius='32px'></Box>
         <Text
-          color='#065F46'
+          color={statusColor ?? '#065F46'}
           fontSize='12px'
           lineHeight='16px'
           fontWeight='500'
@@ -42,11 +49,20 @@ interface AdminHeaderProps {
 }
 
 export function AdminHeader({ connectionCode }: AdminHeaderProps) {
-  const [guardians, setGuardians] = useState<string | undefined>();
+  const [guardiansText, setGuardiansText] = useState<string | undefined>();
+  const [percentOnline, setPercentOnline] = useState<number | undefined>();
+  const [guardiansBackgroundColor, setGuardiansBackgroundColor] = useState<
+    string | undefined
+  >();
+  const [guardiansStatusColor, setGuardiansStatusColor] = useState<
+    string | undefined
+  >();
+
   const {
     state: { configGenParams },
   } = useSetupContext();
   const { api } = useAppContext();
+
   // will have to do polling
   useEffect(() => {
     async function getStatus() {
@@ -54,10 +70,45 @@ export function AdminHeader({ connectionCode }: AdminHeaderProps) {
       const online = consensus ? consensus.peers_online + 1 : 1;
       const offline = consensus ? consensus.peers_offline : 1;
       const totalPeers = online + offline;
-      setGuardians(`${online} / ${totalPeers}`);
+      const percentOnline = online / totalPeers;
+      setPercentOnline(percentOnline);
+      setGuardiansText(`${online} / ${totalPeers}`);
     }
     getStatus();
   }, [api]);
+
+  const greenBG = useColorModeValue('green.400', 'green.900');
+  const greenStatus = useColorModeValue('green.900', 'green.400');
+
+  const yellowBG = useColorModeValue('yellow.400', 'yellow.900');
+  const yellowStatus = useColorModeValue('yellow.900', 'yellow.400');
+
+  const redBG = useColorModeValue('red.400', 'red.900');
+  const redStatus = useColorModeValue('red.900', 'red.400');
+  useEffect(() => {
+    if (!percentOnline) {
+      return;
+    }
+    if (percentOnline == 1) {
+      setGuardiansBackgroundColor(greenBG);
+      setGuardiansStatusColor(greenStatus);
+    } else if (percentOnline >= 2 / 3) {
+      setGuardiansBackgroundColor(yellowBG);
+      setGuardiansStatusColor(yellowStatus);
+    } else {
+      setGuardiansBackgroundColor(redBG);
+      setGuardiansStatusColor(redStatus);
+    }
+  }, [
+    greenBG,
+    greenStatus,
+    percentOnline,
+    redBG,
+    redStatus,
+    yellowBG,
+    yellowStatus,
+  ]);
+
   return (
     <Flex>
       <Box>
@@ -75,7 +126,12 @@ export function AdminHeader({ connectionCode }: AdminHeaderProps) {
           bitcoin standard
         </Text>
         <Flex gap='12px' mt='13px'>
-          <Pill text='Guardians' status={`${guardians}`} />
+          <Pill
+            text='Guardians'
+            status={`${guardiansText}`}
+            statusColor={guardiansStatusColor}
+            backgroundColor={guardiansBackgroundColor}
+          />
           <Pill text='Server' status='Healthy' />
           <Pill text='Uptime' status='100%' />
         </Flex>
